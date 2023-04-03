1dc5f9078a2c9e7f66d2e1d9e371dfc27892690d
DaylenFeist <daylen.feist1@gmail.com>
2023-04-03 13:19:20 -0600

    accel code

--- CAN Code/Teensy_Write/Teensy_Write.ino
+++ CAN Code/Teensy_Write/Teensy_Write.ino
@@ -1,6 +1,13 @@
 #include <FlexCAN_T4.h>
-// #include <kinetis_flexcan.h>
+#include <Wire.h>
+#include "SparkFun_Qwiic_KX13X.h"
+
+//CAN
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can1;
+
+//Accel
+QwiicKX134 kxAccel;
+outputData myData;
 
 CAN_message_t msg;
 const int ledPin = 13;
@@ -11,17 +18,32 @@ CAN_message_t message;
 CAN_message_t rxmsg;
 
 void setup() {
+  
   pinMode(ledPin,OUTPUT);
   Serial.begin(9600);
   digitalWrite(ledPin,HIGH);
   can1.begin();
   can1.setBaudRate(baudRate);
-  
+
+  Wire.begin();
+  kxAccel.begin();
+  kxAccel.initialize(DEFAULT_SETTINGS);
 
 }
 void loop() {
     delay(50);
     can1.read(rxmsg);
+    
+    myData = kxAccel.getAccelData(); 
+    Serial.print("X: ");
+    Serial.print(myData.xData, 4);
+    Serial.print("g ");
+    Serial.print(" Y: ");
+    Serial.print(myData.yData, 4);
+    Serial.print("g ");
+    Serial.print(" Z: ");
+    Serial.print(myData.zData, 4);
+    Serial.println("g ");
     Serial.println(rxmsg.buf[0]);
     if(rxmsg.buf[0] == 49)
     {
